b54bef943da153e48c82f4b13ac7da23dc734dd2
Gab_km <the25thcromosome@gmail.com>
2012-04-10 00:14:29 +0900

    [from now] Tue Apr 10 00:14:29     2012
    diff --git a/src/main/scala/FizzBuzz.scala b/src/main/scala/FizzBuzz.scala
    index 22a7d2d..a63ae86 100644
    --- a/src/main/scala/FizzBuzz.scala
    +++ b/src/main/scala/FizzBuzz.scala
    @@ -2,7 +2,7 @@ object FizzBuzz {
       def fizzbuzz(num: Integer): String = {
         if (num % 3 == 0) {
           "Fizz"
    -    } else if (num == 5) {
    +    } else if (num % 5 == 0) {
           "Buzz"
         } else {
           num.toString

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -2,7 +2,7 @@ object FizzBuzz {
   def fizzbuzz(num: Integer): String = {
     if (num % 3 == 0) {
       "Fizz"
-    } else if (num == 5) {
+    } else if (num % 5 == 0) {
       "Buzz"
     } else {
       num.toString
