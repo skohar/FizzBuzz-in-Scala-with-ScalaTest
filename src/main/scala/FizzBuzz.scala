9f4101b79e894241e27f77f802d64c95ab50af79
Gab_km <the25thcromosome@gmail.com>
2012-04-10 00:22:14 +0900

    Use 'pattern match' instead of if expression.

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,13 +1,10 @@
 object FizzBuzz {
   def fizzbuzz(num: Integer): String = {
-    if (num % 15 == 0) {
-      "FizzBuzz"
-    } else if (num % 3 == 0) {
-      "Fizz"
-    } else if (num % 5 == 0) {
-      "Buzz"
-    } else {
-      num.toString
+    (num % 3, num % 5) match {
+      case (0, 0) => "FizzBuzz"
+      case (0, _) => "Fizz"
+      case (_, 0) => "Buzz"
+      case _      => num.toString
     }
   }
 }
