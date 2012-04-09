0e6ed0a4c025a176deff6341137195e9a161e80a
Gab_km <the25thcromosome@gmail.com>
2012-04-10 00:10:36 +0900

    [from now] Tue Apr 10 00:10:37     2012
    diff --git a/src/main/scala/FizzBuzz.scala b/src/main/scala/FizzBuzz.scala
    index fb57da4..1caed52 100644
    --- a/src/main/scala/FizzBuzz.scala
    +++ b/src/main/scala/FizzBuzz.scala
    @@ -2,6 +2,8 @@ object FizzBuzz {
       def fizzbuzz(num: Integer): String = {
         if (num == 3) {
           "Fizz"
    +    } else if (num == 5) {
    +      "Buzz"
         } else {
           num.toString
         }
    diff --git a/src/test/scala/FizzBuzzTest.scala b/src/test/scala/FizzBuzzTest.scala
    index 8e1a1e3..5af6d68 100644
    --- a/src/test/scala/FizzBuzzTest.scala
    +++ b/src/test/scala/FizzBuzzTest.scala
    @@ -8,5 +8,6 @@ class FizzBuzzTest extends FunSuite {
         assert(fizzbuzz(1) === "1")
         assert(fizzbuzz(2) === "2")
         assert(fizzbuzz(3) === "Fizz")
    +    assert(fizzbuzz(5) === "Buzz")
       }
     }

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -2,6 +2,8 @@ object FizzBuzz {
   def fizzbuzz(num: Integer): String = {
     if (num == 3) {
       "Fizz"
+    } else if (num == 5) {
+      "Buzz"
     } else {
       num.toString
     }
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -8,5 +8,6 @@ class FizzBuzzTest extends FunSuite {
     assert(fizzbuzz(1) === "1")
     assert(fizzbuzz(2) === "2")
     assert(fizzbuzz(3) === "Fizz")
+    assert(fizzbuzz(5) === "Buzz")
   }
 }
