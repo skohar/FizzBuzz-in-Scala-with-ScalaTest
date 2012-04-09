ab7d30fbec2142da6efee411d798437202e67597
Gab_km <the25thcromosome@gmail.com>
2012-04-10 00:13:07 +0900

    [from now] Tue Apr 10 00:13:08     2012
    diff --git a/src/test/scala/FizzBuzzTest.scala b/src/test/scala/FizzBuzzTest.scala
    index 5af6d68..0921093 100644
    --- a/src/test/scala/FizzBuzzTest.scala
    +++ b/src/test/scala/FizzBuzzTest.scala
    @@ -9,5 +9,6 @@ class FizzBuzzTest extends FunSuite {
         assert(fizzbuzz(2) === "2")
         assert(fizzbuzz(3) === "Fizz")
         assert(fizzbuzz(5) === "Buzz")
    +    assert(fizzbuzz(6) === "Fizz")
       }
     }

--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -9,5 +9,6 @@ class FizzBuzzTest extends FunSuite {
     assert(fizzbuzz(2) === "2")
     assert(fizzbuzz(3) === "Fizz")
     assert(fizzbuzz(5) === "Buzz")
+    assert(fizzbuzz(6) === "Fizz")
   }
 }
